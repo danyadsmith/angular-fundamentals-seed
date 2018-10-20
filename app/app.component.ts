803b89ed37f8d3aac886a0e1671a66681f3c4ebf
Danya D. Smith <danyadsmith@gmail.com>
2018-10-20 14:42:20 -0400

    Interpolation and Expressions
    Demonstrate that the values inside string interpolations are expressions
    that will be evaluated like any other JavaScript statement. In them we
    can add, concatenate, and use ternaries to conditionally respond to
    boolean inputs.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,15 +7,23 @@ import { Component } from '@angular/core';
   ],
   template: ` 
     <div class='app'> 
-      {{ title }}
+      {{ title }} <br>
+      {{ numberOne + numberTwo }} <br>
+      {{ isHappy ? ':)' : ':(' }}
     </div> 
   `
 })
 
 export class AppComponent {
   title: string;
+  numberOne: number;
+  numberTwo: number;
+  isHappy: boolean;
 
   constructor() {
     this.title = "Ultimate Angular";
+    this.numberOne = 1;
+    this.numberTwo = 2;
+    this.isHappy = false;
   }
 }
