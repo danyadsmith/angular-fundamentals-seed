85a87edb5668ec52526d2c1b001572c4bdc74695
Danya D. Smith <danyadsmith@gmail.com>
2018-10-20 23:50:45 -0400

    Dynamic @Input Values with *ngFor

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -6,7 +6,6 @@ import { Passenger } from '../../models/passenger.interface';
   selector: 'passenger-count',
   template: `
     <div>
-      <h3>Airline Passengers</h3>
       <div>
         Total checked in: {{ checkedInCount() }} / {{ items.length }}
       </div>
