340b57eb145993a127742eed7630124a56e06c19
Danya D. Smith <danyadsmith@gmail.com>
2018-10-21 20:37:07 -0400

    <select> Option Rendering and ngValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
   selector: 'passenger-form',
@@ -27,6 +28,20 @@ import { Passenger } from '../../models/passenger.interface';
         Check in date:
         <input type="number" name="checkInDate" [ngModel]="detail?.checkInDate">
       </div>
+      <div>
+        Luggage
+        <select name="baggage" [ngModel]="detail?.baggage">
+        <option *ngFor="let item of baggage" [value]="item.key" 
+          [selected]="item.key === detail?.baggage">
+            {{ item.value }}
+          </option>
+        </select>
+        <select name="baggage" [ngModel]="detail?.baggage">
+          <option *ngFor="let item of baggage" [ngValue]="item.key">
+            {{ item.value }}
+          </option>
+        </select>
+      </div>
       {{ form.value | json }}
     </form>
   `
@@ -35,6 +50,25 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+
+  baggage: Baggage[] = [
+    {
+      key: 'none',
+      value: 'No baggage'
+    },
+    {
+      key: 'hand-only',
+      value: 'Carry-on Only'
+    },
+    {
+      key: 'hold-only',
+      value: 'Checked-in Only'
+    },
+    {
+      key: 'hand-hold',
+      value: 'Carry-on and Checked-in'
+    }
+  ];
 
   toggleCheckIn(checkedIn: boolean) {
     if (checkedIn) {
