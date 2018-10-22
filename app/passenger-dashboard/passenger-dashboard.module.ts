e05f8cf2851549e784e1dec24bfd841c82de4703
Danya D. Smith <dsmith04@lear.com>
2018-10-22 10:56:57 -0400

    Feature-Module Routes with forChild()

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
-import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
+import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerViewerComponent } from "./containers/passenger-viewer/passenger-viewer.component";
 
-import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
-import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
-import { PassengerFormComponent } from './components/passenger-form/passenger-form.component';
+import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
+import { PassengerFormComponent } from "./components/passenger-form/passenger-form.component";
 
-import { PassengerDashboardService } from './passenger-dashboard.service';
+import { PassengerDashboardService } from "./passenger-dashboard.service";
+
+const routes: Routes = [
+  { path: "passengers", component: PassengerDashboardComponent }
+];
 
 @NgModule({
   declarations: [
@@ -23,7 +28,8 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     PassengerViewerComponent
