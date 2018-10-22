5cdb106edec17bb9eaddcd4c028f05fe106109ff
Danya D. Smith <dsmith04@lear.com>
2018-10-22 09:51:35 -0400

    Wildcard Routes for 404 Handling

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,9 +7,11 @@ import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashbo
 
 import  { AppComponent } from "./app.component";
 import  { HomeComponent } from "./home.component";
+import  { NotFoundComponent } from "./not-found.component";
 
 const routes: Routes = [
-  { path: "", component: HomeComponent, pathMatch: "full" }
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
@@ -18,7 +20,8 @@ const routes: Routes = [
   ],
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
