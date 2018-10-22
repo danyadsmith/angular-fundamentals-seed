745babfc4b53520289ae5236967121aec3ed21d5
Danya D. Smith <danyadsmith@gmail.com>
2018-10-21 22:54:12 -0400

    Form Validation and Error States

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -10,34 +10,42 @@ import { Baggage } from '../../models/baggage.interface';
     <form #form="ngForm" novalidate>
       {{ detail | json }}
       <div>
-        Passenger ID:
-        <input type="number" name="id" [ngModel]="detail?.id" readonly>
-      </div>
-      <div>
-        Passenger name:
-        <input type="text" name="fullname" [ngModel]="detail?.fullname">
-      </div>
-      <div>
-        Checked In:
-        <label>
-          <input type="checkbox" name="checkedIn" [ngModel]="detail?.checkedIn"
-             (ngModelChange)="toggleCheckIn($event)">
+        <label for="id">
+          Passenger ID:
         </label>
+        <input type="number" name="id" [ngModel]="detail?.id" readonly required
+          #id="ngModel">
+      </div>
+      <div>
+        <label for="name">
+          Passenger name:
+        </label>
+        <input type="text" name="fullname" [ngModel]="detail?.fullname"
+          #fullname="ngModel" required>
+        <div *ngIf="fullname.errors?.required && fullname.dirty" class="error">
+          Passenger name is required
+        </div>
+      </div>
+      <div>
+        <label>
+          Checked In:
+        </label>
+        <input type="checkbox" name="checkedIn" [ngModel]="detail?.checkedIn"
+           (ngModelChange)="toggleCheckIn($event)">
       </div>
       <div *ngIf="form.value.checkedIn">
-        Check in date:
+        <label for="checkInDate">
+           Check in date:
+        </label>
         <input type="number" name="checkInDate" [ngModel]="detail?.checkInDate">
       </div>
       <div>
-        Luggage
+        <label for="baggage">
+          Luggage
+        </label>
         <select name="baggage" [ngModel]="detail?.baggage">
         <option *ngFor="let item of baggage" [value]="item.key" 
           [selected]="item.key === detail?.baggage">
-            {{ item.value }}
-          </option>
-        </select>
-        <select name="baggage" [ngModel]="detail?.baggage">
-          <option *ngFor="let item of baggage" [ngValue]="item.key">
             {{ item.value }}
           </option>
         </select>
